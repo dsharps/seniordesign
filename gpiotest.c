1a48b45b3802da7e9b1b83b338439c608f6d8b8f
Dave <davidrsharples@gmail.com>
2014-02-16 12:37:21 -0500

    Test files

---
+++ gpiotest.c
@@ -0,0 +1,100 @@
+//based off of Gordon Henderson's gpio test code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+// LEDs:
+
+#define GREEN      0
+
+// The input button
+
+#define BUTTON     8
+
+lightIsOn = 0;
+
+/*
+ * setup:
+ *  Program the GPIO correctly and initialise the lamps
+ ***********************************************************************
+ */
+
+void setup (void)
+{
+  int i ;
+
+  if (geteuid () != 0)
+  {
+    fprintf (stderr, "Need to be root to run (sudo?)\n") ;
+    exit (0) ;
+  }
+
+  if (wiringPiSetup () == -1)
+    exit (1) ;
+
+  printf ("Setup ... ") ; fflush (stdout) ;
+  // for (i = 0 ; i < 5 ; ++i)
+  // {
+  //   pinMode (i, OUTPUT) ;
+  //   digitalWrite (i, 0) ;
+  // }
+  // digitalWrite (GREEN, 1) ;
+  // digitalWrite (RED_MAN, 1) ;
+  // pinMode (BUTTON, INPUT) ;
+
+  pinMode (GREEN, OUTPUT);
+  pinMode (BUTTON, INPUT);
+
+  printf ("OK\n") ;
+}
+
+/*
+ * waitButton:
+ *  Wait for the button to be pressed. Because we have the GPIO
+ *  pin pulled high, we wait for it to go low to indicate a push.
+ ***********************************************************************
+ */
+
+void waitButton (void)
+{
+  printf ("Waiting for button ... ") ; fflush (stdout) ;
+  while (digitalRead (BUTTON) == HIGH)
+    delay (100) ;
+  printf ("Got it\n") ;
+}
+
+void toggleLight (void)
+{
+  printf("Toggling the light") ; fflush(stdout) ;
+  if (lightIsOn)
+  {
+    digitalWrite(GREEN, 0);
+    lightIsOn = 0;
+  }
+  else
+  {
+    digitalWrite(GREEN, 1);
+    lightIsOn = 1;
+  }
+}
+
+
+/*
+ ***********************************************************************
+ * The main program
+ *  Call our setup routing once, then sit in a loop, waiting for
+ *  the button to be pressed then executing the sequence.
+ ***********************************************************************
+ */
+
+int main (void)
+{
+  setup () ;
+  for (;;)
+  {
+    waitButton   () ;
+    toggleLight  () ;
+  }
+}
