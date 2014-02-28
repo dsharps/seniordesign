4f70997945583b4fbf2b578c04ef9b1e3d6e27b0
Dave <davidrsharples@gmail.com>
2014-02-28 04:27:31 -0500

    Added pd_test.py

---
+++ pd_test.py
@@ -0,0 +1,39 @@
+import smbus
+import time
+import libpd as pd
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
+
+def readX():
+    x_pos = bus.read_byte(address)
+    return x_pos
+
+def readY():
+    y_pos = bus.read_byte(address)
+    return y_pos
+
+
+
+while True:
+    #print "Reading from Arduino"
+
+    x_low = bus.read_byte(address)
+    x_high = bus.read_byte(address)
+    y_low = bus.read_byte(address)
+    y_high = bus.read_byte(address)
+    
+    #print "x_low: %s, x_high: %s, y_low: %s, y_high: %s" % (x_low, x_high, y_low, y_high)
+
+    print "X: %s, Y: %s" % (x_low + (x_high << 8), y_low + (y_high << 8))
+    # sleep one second
+    time.sleep(0.001)
+
+    #print "----------------"
